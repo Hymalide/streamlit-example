4da504e63e814cfb5a14aca2f4ec942e06ffda15
Hymalide <95407682+Hymalide@users.noreply.github.com>
2023-01-20 15:33:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 data1 = pd.read_csv("group2.csv")
-st.radio("Green","blue")
+st.radio('test bby',("blue"))
 print(st.radio.label)
 
 with st.echo(code_location='below'):
