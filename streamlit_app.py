35e7ff89c5b21f7ec5e973043da88d5216893048
Hymalide <95407682+Hymalide@users.noreply.github.com>
2023-01-21 18:15:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ def show_level(raw_one_alias):
                 list_checked_property = df[checked_property].tolist()
                 index = [i for i in df.index if df.iat[i,1] == raw_one_alias] #We have to remove first column in our csv doc
                 
-                return (raw_one_alias+' = ' + list_checked_property[index[0]]
-        #else:
-                #return(raw_one_alias)
+                return (raw_one_alias+' = ' + list_checked_property[index[0]])
+        else:
+                return(raw_one_alias)
         
 # Create a adio button widget to highlight a quality
 checked_property = st.radio("Choose a property of interest to show each student level up on 10", df.columns.tolist()[1:])
