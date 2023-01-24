4328250d7f6c7459b61109db5f49388652ee081a
Hymalide <95407682+Hymalide@users.noreply.github.com>
2023-01-24 11:58:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,18 @@ This project is part of the course Information Visualization in KTH and uses ano
 
 
 """
+#import plotly.express as px
+
+# continue loading the data with your excel file, I was a bit too lazy to build an Excel file :)
+#df = pd.DataFrame(
+    #[["Product A", 5.6, 7.8, 5], ["Product B", 5.8, 7.2, 4.9]],
+    #columns=["Product", "Comfort", "Sound", "Calls"]
+#)
+
+#fig = px.bar(df, x="Product", y=["Comfort", "Sound", "Calls"], barmode='group', height=400)
+# st.dataframe(df) # if need to display dataframe
+#st.plotly_chart(fig)
+
 
 # Sample data
 data = {'alias': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
